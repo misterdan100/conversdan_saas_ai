2f5da9a7508d17154a12afed777059123b1846bc
misterdan100 <danielmca60@gmail.com>
2025-06-23 05:45:09 -0500

    ui: companion builder form page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,14 @@
+import CompanionForm from "@/components/CompanionForm";
+
 const NewCompanion = () => {
     return (
-        <div>
-            NewCompanion
-        </div>
+        <main className="justify-center items-center min-lg:w-1/3 min-md:w-2/3">
+            <article className="flex flex-col gap-4 w-full">
+                <h1>Companion Builder</h1>
+
+                <CompanionForm />
+            </article>
+        </main>
     );
 };
 
