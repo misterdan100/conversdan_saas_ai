49407aa7dcd7e1dfa51ccf6dd21eecbac12c3027
misterdan100 <danielmca60@gmail.com>
2025-06-25 06:27:18 -0500

    feat: restric features by plan

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,8 @@
 import CompanionForm from "@/components/CompanionForm";
+import { newCompanionPermissions } from '@/lib/actions/companion.actions';
 import { auth } from "@clerk/nextjs/server";
+import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from "next/navigation";
 
 const NewCompanion = async () => {
@@ -7,13 +10,36 @@ const NewCompanion = async () => {
     
     if(!userId) redirect('/sign-in')
 
+    const canCreateCompanion = await newCompanionPermissions()
+
     return (
         <main className="justify-center items-center min-lg:w-1/3 min-md:w-2/3">
-            <article className="flex flex-col gap-4 w-full">
+            { canCreateCompanion ? (
+                <article className="flex flex-col gap-4 w-full">
                 <h1>Companion Builder</h1>
 
                 <CompanionForm />
             </article>
+            ) : (
+                <article className='companion-limit'>
+                    <Image 
+                        src='/images/limit.svg'
+                        alt='Companion limit reached'
+                        width={360}
+                        height={230}
+                    />
+
+                    <div className='cta-badge'>
+                        Upgrade Your Plan
+                    </div>
+
+                    <h1>You've Reached Your Limit</h1>
+                    <p>You've reached your companion limit. Upgrade to create more companions and premium features. </p>
+                    <Link href='/subscription'
+                        className='justify-center w-full btn-primary'
+                    >Upgrade My Plan</Link>
+                </article>
+            )}
         </main>
     );
 };
